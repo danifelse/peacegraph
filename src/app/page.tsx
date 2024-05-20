b291e077c1ff7ba7455f9fe647e5c65581220ddd
danimisme <msubhanr53@gmail.com>
2024-05-20 17:42:24 +0700

    install axios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Navbar from "@/components/navbar";
+import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Header />
       <h1>Peace Graph</h1>
     </div>
   );
