8fe95be6ecd773c0731eddadef1b95f7502be95d
danimisme <msubhanr53@gmail.com>
2024-05-20 13:45:54 +0700

    create navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Navbar from "@/components/navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Peace Graph</h1>
     </div>
   );
