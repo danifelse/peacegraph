af275c317bfdd701ff2e9e89cd54477380152e94
danimisme <msubhanr53@gmail.com>
2024-05-20 23:55:28 +0700

    create function for get data products

--- src/services/getData.ts
+++ src/services/getData.ts
@@ -2,6 +2,19 @@ import instance from "@/lib/axios/instance"
 
 const getData = {
    banner: () => instance.get("/api/banner"),
+   products: () => instance.get("/api/products"),
 }
+
+export async function getBannerData() {
+   const banner = await getData.banner();
+   console.log(banner.data.data);
+   return banner.data.data;
+ }
+
+ export async function getProductsData() {
+   const products = await getData.products();
+   console.log(products.data.data);
+   return products.data.data;
+ }
 
 export default getData
