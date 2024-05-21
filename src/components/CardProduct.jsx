ce99706217b9ba352a83e9f05c6ce37a2ee06eae
danimisme <msubhanr53@gmail.com>
2024-05-21 12:24:17 +0700

    add carousel to product section

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -1,14 +1,64 @@
 export default function CardProduct({ name, price, imageUrl }) {
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="max-w-60 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-3">
       <img className="rounded-t-lg" src={imageUrl} alt="" />
-      <div className="px-5 py-2">
-        <h5 className="mb-1 text-sm font-bold tracking-tight text-gray-900 dark:text-white">
+      <div className="md:px-5 px-1 md:py-2 py-1">
+        <h5 className="mb-1 md:text-sm text-[9px] font-bold tracking-tight text-gray-900 dark:text-white">
           {name}
         </h5>
-        <p className=" text-xs text-gray-700 dark:text-gray-400">
-          Rp. {price?.toLocaleString("id-ID")}
-        </p>
+        <div className="flex justify-between">
+          <p className=" md:text-xs text-[8px] text-gray-700 dark:text-gray-400">
+            Rp. {price?.toLocaleString("id-ID")}
+          </p>
+
+          <div className="flex items-center">
+            <svg
+              className="w-3 h-3 text-yellow-500 ms-1"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 22 20"
+            >
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+            <svg
+              className="w-3 h-3 text-yellow-500 ms-1"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 22 20"
+            >
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+            <svg
+              className="w-3 h-3 text-yellow-500 ms-1"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 22 20"
+            >
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+            <svg
+              className="w-3 h-3 text-yellow-500 ms-1"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 22 20"
+            >
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+            <svg
+              className="w-3 h-3 text-yellow-500 ms-1"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 22 20"
+            >
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/ProductSection.tsx
+++ src/components/ProductSection.tsx
@@ -1,30 +1,25 @@
 import { getProductsData } from "@/services/getData";
-import { Product } from "@/models/Product";
-import CardProduct from "./CardProduct";
+import ProductCarousel from "./ProductCarousel";
 
 export default async function ProductSection() {
   const products = await getProductsData();
   return (
     <div className="container px-4 mb-10">
-      <div className="flex flex-wrap">
-        <div className="w-1/3 lg:px-4 rounded-xl  relative">
-          <div className="rounded-xl overflow-hidden ">
+      <div className="flex  ">
+        <div className="md:w-[40%] w-[30%]  lg:px-4 rounded-xl  relative">
+          <div className="rounded-xl overflow-hidden h-full  ">
             <img
               src="https://images.unsplash.com/photo-1617695744007-68ef55752789?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt=""
-              className="w-full aspect-[3/4] object-cover"
+              className="w-full h-full object-cover"
             />
           </div>
-          <p className="absolute top-3 -right-10 text-xl font-extrabold font-mono inline-block bg-pink-400 px-3 py-2  ">
+          <p className="absolute top-3 -right-10 md:text-xl text-base font-extrabold font-mono inline-block bg-pink-400 md:px-3 md:py-2 p-1 z-10 ">
             Our Product
           </p>
         </div>
-        <div className="w-2/3">
-          <div className="grid grid-cols-3 gap-4">
-            {products.map((product: Product) => (
-              <CardProduct key={product.id} {...product} />
-            ))}
-          </div>
+        <div className="md:w-[60%] w-[70%] ">
+          <ProductCarousel products={products} />
         </div>
       </div>
     </div>
