74072c7e749974b47e5ea35ba424fccc0499d291
danimisme <msubhanr53@gmail.com>
2024-05-21 21:33:34 +0700

    create next-auth handler

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,68 @@
+import { login } from "@/lib/firebase/services"
+import { compare } from "bcrypt"
+import {NextAuthOptions} from "next-auth"
+import NextAuth from "next-auth/next"
+import  CredentialsProvider  from "next-auth/providers/credentials"
+
+const authOptions : NextAuthOptions = {
+    session: {
+        strategy: "jwt"
+    },
+    providers: [
+        CredentialsProvider({
+            type: "credentials",
+            name: "Credentials",
+            credentials: {
+                email: {label: "Email", type: "email"}, 
+                password: {label: "Password", type: "password"}
+            },
+            async authorize(credentials) {
+                const {email, password} = credentials as {email: string, password: string}
+                const user: any = await login(email)
+                if (user) {
+                    const passwordConfirm = await compare(password, user.password)
+                    if(passwordConfirm) {
+                        return user
+                    }
+                    return null
+                } else {
+                    return null
+                }
+            },
+        })
+    ], 
+    callbacks: {
+        jwt: async ({token, account, profile, user}:any) => {
+            if (account?.provider === "Credentials") {
+                token.email = user?.email
+                token.name = user?.name
+                token.role = user?.role
+                 
+            }
+            return token
+        },
+        async session({session, token}:any) {
+            if ("email" in token) {
+                session.user.email = token.email
+                
+            }
+            if ("name" in token) {
+                session.user.name = token.name
+            }
+            if ("role" in token) {
+                session.user.role = token.role
+            }
+            return session
+        },
+     
+    },
+    pages : {
+        signIn: "/auth/login"
+    }
+}
+
+// export default NextAuth(authOptions)
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
