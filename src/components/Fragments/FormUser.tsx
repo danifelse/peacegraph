c68e6275c9f98a617e96ce2296795ffd6f6bfc86
danimisme <msubhanr53@gmail.com>
2024-05-21 19:07:45 +0700

    add message notification

--- src/components/Fragments/FormUser.tsx
+++ src/components/Fragments/FormUser.tsx
@@ -4,12 +4,14 @@ import InputForm from "@/components/Elements/Input";
 import SelectOption from "../Elements/SelectOption";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 export default function FormUser() {
   const { push } = useRouter();
+  const [message, setMessage] = useState("");
+  const [isloading, setIsloading] = useState(false);
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
+    setIsloading(true);
     const form = event.target as HTMLFormElement;
     const userData = {
       name: form.name.value,
@@ -17,6 +19,33 @@ export default function FormUser() {
       email: form.email.value,
       password: form.password.value,
     };
+
+    if (!userData.role) {
+      setMessage("Please select a role");
+      setTimeout(() => {
+        setMessage("");
+        setIsloading(false);
+      }, 2000);
+      return;
+    }
+
+    if (userData.password.length < 6) {
+      setMessage("Password must be at least 6 characters");
+      setTimeout(() => {
+        setMessage("");
+        setIsloading(false);
+      }, 2000);
+      return;
+    }
+
+    if (userData.password !== form.confirmPassword.value) {
+      setMessage("Passwords does not match");
+      setTimeout(() => {
+        setMessage("");
+        setIsloading(false);
+      }, 2000);
+      return;
+    }
 
     console.log(userData);
     const res = await axios.post("/api/user", userData, {
@@ -26,10 +55,16 @@ export default function FormUser() {
         apiKey: process.env.API_KEY?.toString(),
       },
     });
-    if (res.status === 200) {
+    if (res.data.status === 200) {
       form.reset();
       console.log(res.data);
       push("/");
+    } else {
+      setMessage(res.data.message);
+      setTimeout(() => {
+        setMessage("");
+        setIsloading(false);
+      }, 2000);
     }
   };
 
@@ -39,6 +74,14 @@ export default function FormUser() {
         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
           Create an user
         </h1>
+        {message.length > 0 && (
+          <div
+            className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+            role="alert"
+          >
+            <span className="block sm:inline">{message}</span>
+          </div>
+        )}
         <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
           <InputForm
             label="Name"
@@ -74,7 +117,7 @@ export default function FormUser() {
             type="submit"
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
-            Create user
+            {isloading ? "Loading..." : "Create User"}
           </button>
         </form>
       </div>
