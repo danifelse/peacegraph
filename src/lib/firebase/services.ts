47bf0f7be37f371ca11d2f557f7f0c4a83c3e37c
danimisme <msubhanr53@gmail.com>
2024-05-21 16:03:14 +0700

    create function to create user

--- src/lib/firebase/services.ts
+++ src/lib/firebase/services.ts
@@ -1,7 +1,9 @@
 import {} from "firebase/app";
 import app from "./init";
-import { getFirestore, collection, getDocs, query, addDoc } from "firebase/firestore";
+import { getFirestore, collection, getDocs, query, addDoc, where } from "firebase/firestore";
 import { Product } from "@/models/Product";
+import bcrypt from "bcrypt";
+import { User } from "@/models/User";
 
 const firestore = getFirestore(app);
 
@@ -18,4 +20,24 @@ export async function retreiveData(collectionName: string) {
 
 export async function createProduct(product: Product ) {
     await addDoc(collection(firestore, "products"), product);
+}
+
+export async function createUser(userData: User, callback: Function ) {
+    const q = query(
+        collection(firestore, "users"),where("email", "==", userData.email)
+    )
+    const snapshot = await getDocs(q);
+    userData.password = await bcrypt.hash(userData.password, 10);
+    if (snapshot.empty) {
+        await addDoc(collection(firestore, "users"), userData).then(() => {
+            console.log("Document successfully written!");
+            callback(true);
+        }).catch((error) => {
+            console.error("Error writing document: ", error);
+            callback(false);
+        })
+    } else {
+        callback(false);
+        console.log("User already exists");
+    }
 }
