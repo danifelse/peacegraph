5841785465e5fc018ddcc10fa6d1f4dc9ed735a4
danimisme <msubhanr53@gmail.com>
2024-05-22 14:36:02 +0700

    create image handler

--- src/components/Fragments/FormProduct.tsx
+++ src/components/Fragments/FormProduct.tsx
@@ -6,20 +6,72 @@ import { useState } from "react";
 
 export default function FormProduct() {
   const [imageUrl, setImageUrl] = useState("");
+  const [message, setMessage] = useState("");
+  const [isloading, setIsloading] = useState(false);
+  const [imageLoading, setImageLoading] = useState(false);
 
   const handleImageUrl = () => {
+    setImageLoading(true);
     const url = (
       document.querySelector("input[name='imageUrl']") as HTMLInputElement
     )?.value;
-    console.log(url);
     if (url) {
       setImageUrl(url);
     }
   };
 
+  const handleImageError = () => {
+    setIsloading(true);
+    setImageLoading(false);
+    setMessage("Failed to load the image. Please check the URL and try again");
+  };
+
+  const handleImageLoad = () => {
+    setImageLoading(false);
+    setIsloading(false);
+    setMessage("");
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    setIsloading(true);
+    e.preventDefault();
+    const category = e.currentTarget.category.value;
+    if (!category) {
+      setMessage("Please select a category");
+      setTimeout(() => {
+        setIsloading(false);
+        setMessage("");
+      }, 2000);
+      return;
+    }
+    if (imageUrl === "") {
+      setMessage("Please Upload an image");
+      setTimeout(() => {
+        setIsloading(false);
+        setMessage("");
+      }, 2000);
+      return;
+    }
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData.entries());
+    data.category = category;
+    data.imageUrl = imageUrl;
+    for (const key in data) {
+      if (!data[key]) {
+        setMessage(`Please fill in the ${key} field`);
+        setTimeout(() => {
+          setIsloading(false);
+          setMessage("");
+        }, 2000);
+        return;
+      }
+    }
+    console.log(data);
+  };
+
   return (
     <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-3">
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div className="lg:grid grid-cols-2 gap-4 lg:px-10">
           <div className="  space-y-2 md:space-y-4">
             <SelectOption
@@ -59,7 +111,7 @@ export default function FormProduct() {
                 className="text-sm bg-gray-500 hover:bg-gray-700 text-white font-semibold py-1 px-3 rounded-lg absolute right-1 -bottom-10"
                 onClick={handleImageUrl}
               >
-                Upload
+                {imageLoading ? "Loading..." : "Upload"}
               </button>
             </div>
             <div className="border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-3 aspect-square w-1/2 mx-auto relative">
@@ -67,19 +119,24 @@ export default function FormProduct() {
                 src={
                   imageUrl.length > 0 ? imageUrl : "/images/img-placeholder.svg"
                 }
-                alt=""
+                alt="Image"
+                className="w-full h-full object-cover rounded-lg"
+                onError={handleImageError}
+                onLoad={handleImageLoad}
               />
               <p className="text-red-500 text-sm w-full text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-10">
                 Image cannot be uplloaded. <br /> Please check the URL you
                 provided
               </p>
             </div>
-            <div className="flex justify-end">
+
+            <div className="flex justify-end items-center gap-5">
+              {message && <p className="text-red-500 text-sm">{message}</p>}
               <button
                 type="submit"
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
               >
-                Create
+                {isloading ? "Loading..." : "Create"}
               </button>
             </div>
           </div>
