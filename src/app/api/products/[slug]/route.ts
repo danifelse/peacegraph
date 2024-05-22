2ece22b8b557b759cf8eafbed8cc7208a8834623
danimisme <msubhanr53@gmail.com>
2024-05-23 00:53:56 +0700

    create api route for get data product by slug

--- src/app/api/products/[slug]/route.ts
+++ src/app/api/products/[slug]/route.ts
@@ -1,9 +1,21 @@
-import { NextResponse } from "next/server"
+import { retreiveDataBySlug } from "@/lib/firebase/services";
+import { NextRequest, NextResponse } from "next/server"
 
-export async function GET(
-    request: Request,
-    { params }: { params: { slug: string } }
-  ) {
-    const slug = params.slug // 'a', 'b', or 'c'
-    return NextResponse.json({ slug })
+export async function GET( req: NextRequest, { params }: { params: { slug: string } } ) {
+    const apiKey = req.headers.get('apiKey');
+    const validApiKey = process.env.API_KEY
+
+    if (!apiKey || apiKey !== validApiKey) {
+        return NextResponse.json({ error: 'Unauthorized'  }, { status: 401 });
+    }
+    const slug = params.slug
+
+    const data = await retreiveDataBySlug("products", slug);
+
+    if (!data) {
+        return new Response(JSON.stringify({ error: 'Product not found' }), { status: 404, headers: { 'Content-Type': 'application/json' }});
+    }
+
+    return NextResponse.json({ status: 200, message:"Success", data: data });
+
   }
