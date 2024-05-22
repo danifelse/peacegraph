9903d0b1776cb4f85b59f257c46b6b30ca61ee17
danimisme <msubhanr53@gmail.com>
2024-05-23 01:53:35 +0700

    add route to handle update product

--- src/app/api/products/[slug]/route.ts
+++ src/app/api/products/[slug]/route.ts
@@ -1,4 +1,4 @@
-import { retreiveDataBySlug } from "@/lib/firebase/services";
+import { retreiveDataBySlug, updateProduct } from "@/lib/firebase/services";
 import { NextRequest, NextResponse } from "next/server"
 
 export async function GET( req: NextRequest, { params }: { params: { slug: string } } ) {
@@ -19,3 +19,22 @@ export async function GET( req: NextRequest, { params }: { params: { slug: strin
     return NextResponse.json({ status: 200, message:"Success", data: data });
 
   }
+
+
+export async function PUT( req: NextRequest, { params }: { params: { slug: string } } ) {
+    const apiKey = req.headers.get('apiKey');
+    const validApiKey = process.env.API_KEY
+
+    if (!apiKey || apiKey !== validApiKey) {
+        return NextResponse.json({ error: 'Unauthorized'  }, { status: 401 });
+    }
+    const slug = params.slug
+    const newData = await req.json();
+
+    const status = await updateProduct(slug, newData);
+    if (status) {
+        return NextResponse.json({ status: 200, message: 'Product updated successfully', data: newData });
+    } else {
+        return new Response(JSON.stringify({ error: 'Failed to update product' }), { status: 404, headers: { 'Content-Type': 'application/json' }});
+    }
+} 
