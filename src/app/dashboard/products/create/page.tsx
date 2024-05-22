33a4dde10f242759a4ba24bd26fd1715e3094dec
danimisme <msubhanr53@gmail.com>
2024-05-22 20:10:36 +0700

    add link back to products list

--- src/app/dashboard/products/create/page.tsx
+++ src/app/dashboard/products/create/page.tsx
@@ -3,7 +3,9 @@
 import FormProduct from "@/components/Fragments/FormProduct";
 import { Product } from "@/models/Product";
 import { postData } from "@/services/postDataClient";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { FaArrowLeftLong } from "react-icons/fa6";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -22,8 +24,17 @@ export default function CreateProduct() {
   };
   return (
     <div className="p-4 sm:ml-64">
-      <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-        <h1>Create Product</h1>
+      <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14 flex items-center justify-between">
+        <h1 className="text-xl font-semibold mb-2">Create Product</h1>
+        <Link
+          href="/dashboard/products"
+          className="text-blue-500 px-3 hover:text-pink-500"
+        >
+          <span>
+            <FaArrowLeftLong className="h-6 w-6 me-3 inline-block" />
+          </span>
+          Back to Products list
+        </Link>
       </div>
       <FormProduct onSubmitForm={handleCreate} />
       <ToastContainer
