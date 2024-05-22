dd3ddb2a5ac7aad2aef455e50e7a14e170d536fa
danimisme <msubhanr53@gmail.com>
2024-05-23 02:07:37 +0700

    Add toast container

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import NavbarDashboard from "@/components/Layouts/NavbarDashboard";
 import Sidebar from "@/components/Layouts/Sidebar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -7,6 +9,18 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <NavbarDashboard />
       <Sidebar />
       <div>{children}</div>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </div>
   );
 }
