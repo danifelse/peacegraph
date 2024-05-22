ebf2a2c5349292aadbac6e70a6778de1658d1c45
danimisme <msubhanr53@gmail.com>
2024-05-22 15:24:50 +0700

    add toast notification

--- src/app/dashboard/products/create/page.tsx
+++ src/app/dashboard/products/create/page.tsx
@@ -3,13 +3,20 @@
 import FormProduct from "@/components/Fragments/FormProduct";
 import { Product } from "@/models/Product";
 import { postData } from "@/services/postDataClient";
+import { useRouter } from "next/navigation";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function CreateProduct() {
+  const { push } = useRouter();
   const handleCreate = async (data: Product) => {
     try {
       const res = await postData("/api/products", data);
+      toast.success("Product created successfully");
+      setTimeout(() => push("/dashboard/products"), 2000);
       return res;
     } catch (error) {
+      toast.error("Failed to create product");
       return error;
     }
   };
@@ -19,6 +26,18 @@ export default function CreateProduct() {
         <h1>Create Product</h1>
       </div>
       <FormProduct onSubmitForm={handleCreate} />
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </div>
   );
 }
