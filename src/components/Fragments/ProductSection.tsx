a05950425802d3ab274e25d92f6870bd6d7cdb10
danimisme <msubhanr53@gmail.com>
2024-05-24 02:24:31 +0700

    create product section

---
+++ src/components/Fragments/ProductSection.tsx
@@ -0,0 +1,22 @@
+import { getCategoriesData, getProductsData } from "@/services/getData";
+import CategorySection from "./CategorySection";
+import { Product } from "@/models/Product";
+import { Category } from "@/models/Category";
+
+export default async function ProductSection() {
+  const products: Product[] = await getProductsData();
+  const categories: Category[] = await getCategoriesData();
+
+  return (
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 ">
+      {categories.map((category, index) => (
+        <CategorySection
+          key={category.id}
+          products={products}
+          category={category}
+          index={index}
+        />
+      ))}
+    </div>
+  );
+}
