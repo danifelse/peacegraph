03c1e2bee827f8310d5c907edd278100ebd6adf5
danimisme <msubhanr53@gmail.com>
2024-05-23 21:49:00 +0700

    create route to handle delete category

--- src/app/api/categories/[slug]/route.ts
+++ src/app/api/categories/[slug]/route.ts
@@ -1,4 +1,4 @@
-import { retreiveDataBySlug, updateCategory } from "@/lib/firebase/services";
+import { deleteCategory, retreiveDataBySlug, updateCategory } from "@/lib/firebase/services";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, { params }: { params: { slug: string } }) {
@@ -28,5 +28,20 @@ export async function PUT ( req: NextRequest, { params }: { params: { slug: stri
         return NextResponse.json({ status: 200, message: 'Category updated successfully', data: newData });
     } else {
         return new Response(JSON.stringify({ error: 'Failed to update category' }), { status: 404, headers: { 'Content-Type': 'application/json' } });
+    }
+}
+
+export async function DELETE ( req: NextRequest, { params }: { params: { slug: string } } ) {
+    const apiKey = req.headers.get('apiKey');
+    const validApiKey = process.env.API_KEY;
+    if (!apiKey || apiKey !== validApiKey) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+    const slug = params.slug;
+    const status = await deleteCategory(slug);
+    if (status) {
+        return NextResponse.json({ status: 200, message: 'Category deleted successfully' });
+    } else {
+        return new Response(JSON.stringify({ error: 'Failed to delete category' }), { status: 404, headers: { 'Content-Type': 'application/json' } });
     }
 }
