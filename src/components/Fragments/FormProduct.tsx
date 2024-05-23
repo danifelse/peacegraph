81fabef77163d07e906b2063a08d30707b88e675
danimisme <msubhanr53@gmail.com>
2024-05-23 22:11:12 +0700

    disable button when isLoading true

--- src/components/Fragments/FormProduct.tsx
+++ src/components/Fragments/FormProduct.tsx
@@ -197,6 +197,7 @@ export default function FormProduct({
               <button
                 type="submit"
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5"
+                disabled={isloading}
               >
                 {isloading
                   ? "Loading..."
