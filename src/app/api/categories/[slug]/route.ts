6c0f6810173c2e1a79c1cd9537e360e002933772
danimisme <msubhanr53@gmail.com>
2024-05-23 21:32:14 +0700

    create route to handle categories update

--- src/app/api/categories/[slug]/route.ts
+++ src/app/api/categories/[slug]/route.ts
@@ -1,7 +1,7 @@
-import { retreiveDataBySlug } from "@/lib/firebase/services";
-import { NextResponse } from "next/server";
+import { retreiveDataBySlug, updateCategory } from "@/lib/firebase/services";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: Request, { params }: { params: { slug: string } }) {
+export async function GET(req: NextRequest, { params }: { params: { slug: string } }) {
     const apiKey = req.headers.get('apiKey');
     const validApiKey = process.env.API_KEY;
     if (!apiKey || apiKey !== validApiKey) {
@@ -13,4 +13,20 @@ export async function GET(req: Request, { params }: { params: { slug: string } }
         return new Response(JSON.stringify({ error: 'Category not found' }), { status: 404, headers: { 'Content-Type': 'application/json' } });
     }
     return NextResponse.json({ status: 200, message:"Success", data: data });
+}
+
+export async function PUT ( req: NextRequest, { params }: { params: { slug: string } } ) {
+    const apiKey = req.headers.get('apiKey');
+    const validApiKey = process.env.API_KEY;
+    if (!apiKey || apiKey !== validApiKey) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+    const slug = params.slug;
+    const newData = await req.json();
+    const status = await updateCategory(slug, newData);
+    if (status) {
+        return NextResponse.json({ status: 200, message: 'Category updated successfully', data: newData });
+    } else {
+        return new Response(JSON.stringify({ error: 'Failed to update category' }), { status: 404, headers: { 'Content-Type': 'application/json' } });
+    }
 }
