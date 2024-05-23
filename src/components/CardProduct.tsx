37801eaf26a86e7d8cf25dc687fda4a8abd4b483
danimisme <msubhanr53@gmail.com>
2024-05-24 00:42:04 +0700

    modif style of card product

---
+++ src/components/CardProduct.tsx
@@ -0,0 +1,85 @@
+import Link from "next/link";
+
+export default function CardProduct({
+  name,
+  slug,
+  price,
+  imageUrl,
+}: {
+  name: string;
+  slug: string;
+  price: number;
+  imageUrl: string;
+}) {
+  return (
+    <Link href={`/product/${slug}`} className="group">
+      <div className="max-w-60 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 m-1 lg:m-3 group-hover:-translate-y-2 group-hover:shadow-none transition-all duration-500 overflow-hidden">
+        <div className="overflow-hidden ">
+          <img
+            className="rounded-t-lg aspect-square object-cover object-center group-hover:scale-110 transition-all duration-500"
+            src={imageUrl}
+            alt=""
+          />
+        </div>
+        <div className="md:px-5 px-1 md:py-2 py-1 bg-gray-50 group-hover:bg-white">
+          <h5 className="mb-1 md:text-sm text-[9px] font-bold tracking-tight text-gray-900 dark:text-white">
+            {name}
+          </h5>
+          <div className="flex justify-between">
+            <p className=" md:text-xs text-[8px] text-gray-700 dark:text-gray-400">
+              Rp. {price?.toLocaleString("id-ID")}
+            </p>
+
+            <div className="flex items-center">
+              <svg
+                className="lg:w-3 lg:h-3 w-2 text-yellow-500 md:ms-1"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 22 20"
+              >
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+              </svg>
+              <svg
+                className="lg:w-3 lg:h-3 w-2 text-yellow-500 md:ms-1"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 22 20"
+              >
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+              </svg>
+              <svg
+                className="lg:w-3 lg:h-3 w-2 text-yellow-500 md:ms-1"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 22 20"
+              >
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+              </svg>
+              <svg
+                className="lg:w-3 lg:h-3 w-2 text-yellow-500 md:ms-1"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 22 20"
+              >
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+              </svg>
+              <svg
+                className="lg:w-3 lg:h-3 w-2 text-yellow-500 md:ms-1"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 22 20"
+              >
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
