7df2e2117b70ee6169a71690afbc94ccacc6d800
danimisme <msubhanr53@gmail.com>
2024-05-23 17:21:16 +0700

    create funtion delete product

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,18 +1,38 @@
 "use client";
 import DashboardCardProduct from "@/components/DashboardCardProduct";
+import ModalDelete from "@/components/Modals/ModalDelete";
+import { useAppSelector } from "@/lib/redux/hooks";
 import { Product } from "@/models/Product";
+import { deleteData } from "@/services/deleteDataClient";
 import { getData } from "@/services/getDataClient";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { IoMdAddCircleOutline } from "react-icons/io";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Products() {
+  const modalState = useAppSelector((state) => state.modalDelete);
+  console.log(modalState);
+
   const [products, setProducts] = useState<Product[]>([]);
   useEffect(() => {
     getData("/api/products")
       .then((res) => setProducts(res.data.data))
       .catch((err) => console.log(err));
   }, []);
+
+  const deleteProduct = async (slug: string) => {
+    console.log(`page product slug : ${slug}`);
+    const res = await deleteData(`/api/products/${slug}`);
+    if (res.status === 200) {
+      toast.success(res.data.message);
+      setProducts(products.filter((product) => product.slug !== slug));
+    } else {
+      toast.error(res.response.data.error);
+    }
+  };
+
   return (
     <div className="p-4 sm:ml-64">
       <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
@@ -39,6 +59,19 @@ export default function Products() {
             <DashboardCardProduct key={product.id} {...product} />
           ))}
       </div>
+      <ModalDelete modalState={modalState} handleConfirm={deleteProduct} />
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </div>
   );
 }
