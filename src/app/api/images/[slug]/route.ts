d6676aeda35f873b1bc627549f40178673d10861
danimisme <msubhanr53@gmail.com>
2024-05-24 18:01:11 +0700

    create routes to handle images fetching

---
+++ src/app/api/images/[slug]/route.ts
@@ -0,0 +1,37 @@
+import { getJSON } from "@/lib/firebase/servicejson";
+import { NextRequest, NextResponse } from "next/server";
+
+interface ImageData {
+    id: string;
+    slug: string;
+    title: string;
+    subtitle: string;
+     desc: string
+}
+
+export async function GET(req: NextRequest, { params }: { params: { slug: string } }) {
+    const apiKey = req.headers.get('apiKey');
+    const validApiKey = process.env.API_KEY;
+
+    if (!apiKey || apiKey !== validApiKey) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    const slug = params.slug;
+
+    try {
+        const data  = await getJSON("images");
+        const imagesData : ImageData[] = JSON.parse(data.imageData);
+        
+        const imageData = imagesData.find((image: ImageData) => image.slug === slug);
+
+        if (!imageData) {
+            return NextResponse.json({ error: 'Image not found' }, { status: 404 });
+        }
+
+        return NextResponse.json({ status: 200, message: "Success", data: imageData });
+    } catch (error) {
+        console.error("Error fetching data from Firestore", error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+}
