573da682c9de84cdd573fab59ad9dfd464970a01
danimisme <msubhanr53@gmail.com>
2024-05-24 21:17:23 +0700

    add funtion to get images data

--- src/services/getData.ts
+++ src/services/getData.ts
@@ -1,15 +1,17 @@
 import instance from "@/lib/axios/instance"
 
 const getData = {
-   banner: () => instance.get("/api/banner"),
+   images: () => instance.get("/api/images"),
    products: () => instance.get("/api/products"),
    categories: () => instance.get("api/categories"),
 }
 
-export async function getBannerData() {
-   const banner = await getData.banner();
-   return banner.data.data;
+export async function getImages() {
+   const images = await getData.images();
+   return images.data.data;
  }
+
+
 
  export async function getProductsData() {
    const products = await getData.products();
