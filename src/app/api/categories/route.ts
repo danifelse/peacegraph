39964216cc9c9ac380abb9d20402ee8fcfa8a1ab
danimisme <msubhanr53@gmail.com>
2024-05-25 04:04:21 +0700

    modif logic of route categories

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,43 +1,56 @@
-import { createCollectionData, getCollectionData } from "@/lib/firebase/servicejson";
+import { createCollectionData, getCollectionData, getJSON, updateJSON } from "@/lib/firebase/servicejson";
 import { createCategory, retreiveData } from "@/lib/firebase/services";
+import { Category } from "@/models/Category";
 import { NextRequest, NextResponse } from "next/server";
 
 
-//GET CATEGORY from categorieswithproducts collection
-export async function GET(req: NextRequest){
+export async function GET(req: NextRequest, ) {
     const apiKey = req.headers.get('apiKey');
-    const validApiKey = process.env.API_KEY
+    const validApiKey = process.env.API_KEY;
 
     if (!apiKey || apiKey !== validApiKey) {
-        return NextResponse.json({ error: 'Unauthorized'  }, { status: 401 });
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
     }
-    const data = await getCollectionData("categorieswithproducts");
-    return NextResponse.json({ status: 200, message:"Success", data: data }); 
-}
-
-export async function POST(req: NextRequest){
     try {
-    const apiKey = req.headers.get('apiKey');
-    const validApiKey = process.env.API_KEY
-
-    if (!apiKey || apiKey !== validApiKey) {
-        return NextResponse.json({ error: 'Unauthorized'  }, { status: 401 });
-    }
-
-    const data = await req.json();
-    if (data) {
-        const status = await createCollectionData("categorieswithproducts",data);
-        if (status) {
-            return NextResponse.json({ status: 200, message: 'Category created successfully' });
-        } else {
-            return new Response(JSON.stringify({ error: 'Category already exists' }), { status: 400, headers: { 'Content-Type': 'application/json' }});
+        const data  = await getJSON("categoriesjson");
+        const categoriesData : Category[] = JSON.parse(data.categoriesData);
+        
+        if (!categoriesData) {
+            return NextResponse.json({ error: 'Images not found' }, { status: 404 });
         }
 
-    } else { 
-        return new Response(JSON.stringify({ error: 'Invalid JSON' }), { status: 400, headers: { 'Content-Type': 'application/json' }});
-    }
+        return NextResponse.json({ status: 200, message: "Success", data: categoriesData });
     } catch (error) {
-        return new Response(JSON.stringify({ error: error }), { status: 400, headers: { 'Content-Type': 'application/json' }});
+        console.error("Error fetching data from Firestore", error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+}
+
+
+export async function POST(req: NextRequest  ) {
+    const apiKey = req.headers.get('apiKey');
+    const validApiKey = process.env.API_KEY;
+
+    if (!apiKey || apiKey !== validApiKey) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+    try {
+        const data  = await getJSON("categoriesjson");
+        const categoriesData : Category[] = JSON.parse(data.categoriesData);
+        const newData = await req.json();
+        
+        categoriesData.push(newData);
+        data.categoriesData = JSON.stringify(categoriesData);
+        if (newData) {
+            await updateJSON("categoriesjson", "categories" , data );
+            return NextResponse.json({ status: 200, message: `Success Create ${newData.name} category` , data: newData });
+        }
+
+        return NextResponse.json({ error: 'Data not found' }, { status: 404 });
+
+    } catch (error) {
+        console.error("Error fetching data from Firestore", error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
     }
 }
 
