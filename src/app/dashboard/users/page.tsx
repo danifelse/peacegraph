8395120b4a3f579740d05900eadc47e0d058a244
danimisme <msubhanr53@gmail.com>
2024-05-26 22:46:17 +0700

    add permission to manage user

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -5,14 +5,18 @@ import UsersTable from "@/components/Tables/UsersTable";
 import { useAppSelector } from "@/lib/redux/hooks";
 import { deleteData } from "@/services/deleteDataClient";
 import { getData } from "@/services/getDataClient";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { CiLock } from "react-icons/ci";
 import { IoMdAddCircleOutline } from "react-icons/io";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function User() {
+  const { data: session } = useSession();
+
   const modalState = useAppSelector((state) => state.modalDelete);
   const [users, setUsers] = useState<any>([]);
 
@@ -42,7 +46,27 @@ export default function User() {
         <div className="bg-blue-500 px-10 py-5 rounded-xl flex items-center justify-between">
           <div>
             <h1 className="text-white text-5xl mb-2">Users</h1>
-            {users.length > 0 ? (
+            {session?.user?.role === "super-admin" && users.length === 0 && (
+              <div className="flex items-center gap-2">
+                <AiOutlineLoading3Quarters className="text-white animate-spin" />
+                <span className="text-white">Loading...</span>
+              </div>
+            )}
+            {session?.user?.role === "super-admin" && users.length > 0 && (
+              <p className="text-white  mb-1">
+                {users.length} Users are available
+              </p>
+            )}
+            {session?.user?.role !== "super-admin" && (
+              <div className="flex gap-2 items-center justify-center text-white animate-pulse  ">
+                <span>
+                  <CiLock />
+                </span>
+                <p className="">Unauthorized</p>
+              </div>
+            )}
+
+            {/* {users.length > 0 && session?.user?.role === "super-admin" ? (
               <p className="text-white  mb-1">
                 {users.length} Users are available
               </p>
@@ -51,23 +75,34 @@ export default function User() {
                 <AiOutlineLoading3Quarters className="text-white animate-spin" />
                 <span className="text-white">Loading...</span>
               </div>
-            )}
+            )} */}
           </div>
-          <Link href="/dashboard/users/create">
-            <button className="bg-transparent  hover:bg-blue-500 text-white font-semibold hover:text-blue-700 hover:bg-white hover:border-blue-800 py-2 px-4 border-2 border-white hover:border-transparent rounded-xl flex items-center gap-2">
-              <span>
-                <IoMdAddCircleOutline className="h-6 w-6" />
-              </span>
-              Add User
-            </button>
-          </Link>
+          {session?.user?.role === "super-admin" && (
+            <Link href="/dashboard/users/create">
+              <button className="bg-transparent  hover:bg-blue-500 text-white font-semibold hover:text-blue-700 hover:bg-white hover:border-blue-800 py-2 px-4 border-2 border-white hover:border-transparent rounded-xl flex items-center gap-2">
+                <span>
+                  <IoMdAddCircleOutline className="h-6 w-6" />
+                </span>
+                Add User
+              </button>
+            </Link>
+          )}
         </div>
       </div>
       <div className=" mt-3 p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
-        {users.length > 0 ? (
+        {users.length > 0 && session?.user?.role === "super-admin" ? (
           <UsersTable users={users} />
         ) : (
           <div className="">
+            <div className="flex gap-2 items-center justify-center p-4  ">
+              <span>
+                <CiLock />
+              </span>
+              <p className="text-gray-500">
+                You do not have permission to manage users. Please contact the
+                super-admin to make changes.
+              </p>
+            </div>
             <SkeletonList color="blue" />
           </div>
         )}
