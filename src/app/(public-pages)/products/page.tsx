87b24458a5e425ef44acf3392bd42cc8ecbc7283
danimisme <msubhanr53@gmail.com>
2024-05-27 02:02:01 +0700

    add product container

--- src/app/(public-pages)/products/page.tsx
+++ src/app/(public-pages)/products/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import BannerProducts from "@/components/Fragments/BannerProducts";
+import ProductsContainer from "@/components/Fragments/ProductsContainer";
 
 export default function Page() {
   return (
     <div className="mt-20">
       <BannerProducts />
+      <ProductsContainer />
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8"></div>
     </div>
   );
