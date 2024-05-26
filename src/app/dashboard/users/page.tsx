e2bf369a4600e41804b53b410275801b0edd87da
danimisme <msubhanr53@gmail.com>
2024-05-26 21:41:26 +0700

    fix create user link

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -41,7 +41,7 @@ export default function User() {
               {users.length} Users are available
             </p>
           </div>
-          <Link href="/dashboard/user/create">
+          <Link href="/dashboard/users/create">
             <button className="bg-transparent  hover:bg-blue-500 text-white font-semibold hover:text-blue-700 hover:bg-white hover:border-blue-800 py-2 px-4 border-2 border-white hover:border-transparent rounded-xl flex items-center gap-2">
               <span>
                 <IoMdAddCircleOutline className="h-6 w-6" />
