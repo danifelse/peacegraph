7c6348e0bc715dca3c7a0ab265805d52b48dd8d5
danimisme <msubhanr53@gmail.com>
2024-05-26 19:48:12 +0700

    create fecther function

--- src/lib/axios/instance.ts
+++ src/lib/axios/instance.ts
@@ -4,14 +4,14 @@ const headers = {
     Accept: "application/json",
     "Content-Type": "application/json",
     "Cahce-Control": "no-cache",
+    "apiKey": process.env.NEXT_PUBLIC_KEY,
     Expires: 0,
+    
 }
 
 const instance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-    headers: {
-        "apiKey": process.env.API_KEY,
-    },
+    headers: headers,
     timeout: 60* 1000
 });
 
@@ -24,5 +24,18 @@ instance.interceptors.request.use(
     (response) => response,
     (error) => Promise.reject(error),
 )
+export const fetcher = (url : string) => {
+    console.log(url)
+    return instance.get(url).then((res) => {
+        console.log(res)
+      if (!res.data) {
+        throw Error(res.data.message);
+      }
+
+      return res.data;
+    }).catch((err) => {
+        console.log(err)
+    })
+  };
 
 export default instance
