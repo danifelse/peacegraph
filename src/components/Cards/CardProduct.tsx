b75a65b7a303c4fe7284af3b09706a86cb0bb7da
danimisme <msubhanr53@gmail.com>
2024-05-28 00:27:45 +0700

    fix category link card

--- src/components/Cards/CardProduct.tsx
+++ src/components/Cards/CardProduct.tsx
@@ -4,15 +4,17 @@ export default function CardProduct({
   name,
   slug,
   price,
+  category,
   imageUrl,
 }: {
   name: string;
   slug: string;
   price: number;
+  category: string;
   imageUrl: string;
 }) {
   return (
-    <Link href={`/product/${slug}`} className="group">
+    <Link href={`/products/${category}/${slug}`} className="group">
       <div className="max-w-60 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 m-1 lg:m-3 group-hover:-translate-y-2 group-hover:shadow-none transition-all duration-500 overflow-hidden">
         <div className="overflow-hidden ">
           <img
