334f2ad6fbf81d93ae7d6257da5e7846d66327b6
danimisme <msubhanr53@gmail.com>
2024-05-27 13:21:55 +0700

    modif category container in products page '

--- src/components/Fragments/ProductsContainer.tsx
+++ src/components/Fragments/ProductsContainer.tsx
@@ -42,40 +42,42 @@ export default function ProductsContainer() {
   return (
     <div className="lg:-mt-28 md:-mt-40 -mt-20 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 z-10 relative ">
       <div className="bg-white  rounded-lg grid lg:grid-cols-4 md:grid-cols-4 grid-cols-1 ">
-        <div className="lg:p-10 md:p-6 p-2 md:sticky md:top-32 md:max-w-sm w-full max-h-96 bg-white shadow-xl rounded-lg ">
-          <h1 className="text-3xl text-gray-700">Category</h1>
-          <div className="mt-4 grid grid-cols-2 md:grid-cols-1 gap-2 md:gap-0 ">
-            {categories.map((category) => (
-              <div
-                key={category.slug}
-                className=" flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700 relative"
-              >
-                <div className="flex items-center ">
-                  <input
-                    type="checkbox"
-                    id={category.slug}
-                    name={category.slug}
-                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    value={category.slug}
-                    defaultChecked
-                    onChange={handleChange}
-                  />
-                  <label
-                    htmlFor={category.slug}
-                    className="w-full py-4 ms-2 md:text-sm text-xs font-medium text-gray-900 dark:text-gray-300 pe-10"
-                  >
-                    {category.name}
-                    <span className="md:absolute md:right-5 px-2 py-0.5 text-xs font-medium tracking-wide text-blue-500 bg-blue-100 rounded">
-                      {
-                        products.filter(
-                          (product) => product.category === category.slug
-                        ).length
-                      }
-                    </span>
-                  </label>
+        <div className="   rounded-lg ">
+          <div className="lg:p-10 md:p-6 p-2 md:sticky md:top-20 md:max-w-sm w-full bg-whgite shadow-xl rounded-lg">
+            <h1 className="text-3xl text-gray-700">Category</h1>
+            <div className="mt-4 grid grid-cols-2 md:grid-cols-1 gap-2 md:gap-0 ">
+              {categories.map((category) => (
+                <div
+                  key={category.slug}
+                  className=" flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700 relative"
+                >
+                  <div className="flex items-center ">
+                    <input
+                      type="checkbox"
+                      id={category.slug}
+                      name={category.slug}
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      value={category.slug}
+                      defaultChecked
+                      onChange={handleChange}
+                    />
+                    <label
+                      htmlFor={category.slug}
+                      className="w-full py-4 ms-2 md:text-sm text-xs font-medium text-gray-900 dark:text-gray-300 pe-10"
+                    >
+                      {category.name}
+                      <span className="md:absolute md:right-5 px-2 py-0.5 text-xs font-medium tracking-wide text-blue-500 bg-blue-100 rounded">
+                        {
+                          products.filter(
+                            (product) => product.category === category.slug
+                          ).length
+                        }
+                      </span>
+                    </label>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
         <div className="col-span-3 lg:pe-10">
