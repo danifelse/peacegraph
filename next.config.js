78b6f230c740027ff210d66d3bde7f2bf0c9f723
danimisme <msubhanr53@gmail.com>
2024-05-28 12:27:18 +0700

    change next js config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://peacegraph.vercel.app/api/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
