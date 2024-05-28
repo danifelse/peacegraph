dd43a34bb10f21a42d3b8947c0d820067089dc04
danimisme <msubhanr53@gmail.com>
2024-05-28 21:55:50 +0700

    change style of article container

--- src/components/Fragments/ArticlesContainer.tsx
+++ src/components/Fragments/ArticlesContainer.tsx
@@ -3,6 +3,7 @@ import { Article } from "@/models/Articles";
 import { getData } from "@/services/getDataClient";
 import { useEffect, useState } from "react";
 import SkeletonList from "./SkeletonList";
+import Skeleton from "./Skeleton";
 
 export default function ArticlesContainer() {
   const [articles, setArticles] = useState<Article[]>([]);
@@ -21,16 +22,24 @@ export default function ArticlesContainer() {
     <div className="lg:-mt-28 md:-mt-40 -mt-20 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative ">
       <div className="bg-white shadow-lg  rounded-lg grid lg:grid-cols-4 md:grid-cols-4 grid-cols-1 ">
         <div className="   rounded-lg ">
-          <div className="lg:p-10 md:p-6 p-2 md:sticky md:top-20 md:max-w-sm w-full bg-whgite  rounded-lg">
-            <h1 className="text-3xl text-gray-700">Article</h1>
-            <div className="mt-4 grid grid-cols-2 md:grid-cols-1 gap-2 md:gap-0 ">
-              <div className="flex flex-col gap-1 ">
-                {sortedArticles.length === 0 && <SkeletonList />}
+          <div className=" md:sticky md:top-20 md:max-w-sm w-full bg-whgite  rounded-lg">
+            <div className="mt-4 lg:px-10 lg:pt-4 md:px-6 md:pt-2 ">
+              <h1 className="text-3xl text-gray-700 ">Article</h1>
+            </div>
+            <div className=" lg:px-10 lg:py-4 md:px-6 md:py-2 p-2  grid grid-cols-2 md:grid-cols-1 gap-2 md:gap-0  max-h-screen overflow-y-auto">
+              <div className="space-y-2 ">
+                {sortedArticles.length === 0 && (
+                  <div className="px-3 py-2 rounded-lg overflow-y-auto">
+                    <SkeletonList />
+                  </div>
+                )}
                 {sortedArticles.map((article, i) => (
                   <div
                     key={i}
-                    className={`px-3 py-2 rounded-lg cursor-pointer  hover:bg-pink-500 hover:text-white ${
-                      indexOpen === i ? "bg-pink-500 text-white" : "bg-pink-200"
+                    className={`px-3 py-2 rounded-lg cursor-pointer transition duration-300 ${
+                      indexOpen === i
+                        ? "-translate-x-2 bg-pink-500 text-white shadow-md"
+                        : "  bg-pink-200 hover:-translate-x-2"
                     }`}
                     onClick={() => setIndexOpen(i)}
                   >
@@ -41,7 +50,12 @@ export default function ArticlesContainer() {
             </div>
           </div>
         </div>
-        <div className="col-span-3 lg:px-12 relative py-12 shadow-md">
+        <div className="col-span-3 lg:px-12 relative py-12 shadow-md min-h-screen ">
+          {sortedArticles.length === 0 && (
+            <div>
+              <SkeletonList />
+            </div>
+          )}
           <div
             className="prose max-w-full"
             dangerouslySetInnerHTML={{
