0e219da8b18c47e9c57186d04bdc04911774e58e
danimisme <msubhanr53@gmail.com>
2024-05-28 22:39:04 +0700

    delete aos in products section

--- src/components/Fragments/ProductSection.tsx
+++ src/components/Fragments/ProductSection.tsx
@@ -4,13 +4,10 @@ import { Product } from "@/models/Product";
 import { Category } from "@/models/Category";
 import { useGetData } from "@/lib/swr/hooks";
 import Skeleton from "./Skeleton";
-import { AOSInit } from "@/lib/aos/aos";
 
 export default function ProductSection() {
   const productsData = useGetData("api/products");
   const categoriesData = useGetData("api/categories");
-
-  AOSInit();
 
   const products: Product[] = productsData?.data?.data;
   const categories: Category[] = categoriesData?.data?.data;
@@ -26,7 +23,7 @@ export default function ProductSection() {
   }
 
   return (
-    <div className="mx-auto max-w-7xl px-4 sm:px-6 " data-aos="fade-up">
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 ">
       {categories.map((category, index) => (
         <CategorySection
           key={category.slug}
