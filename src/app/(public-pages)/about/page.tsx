8c5f0664942246aebad5c926285e3851b2260b40
danimisme <msubhanr53@gmail.com>
2024-05-28 20:58:52 +0700

    change metadata of about page

--- src/app/(public-pages)/about/page.tsx
+++ src/app/(public-pages)/about/page.tsx
@@ -1,6 +1,11 @@
 import ArticlesContainer from "@/components/Fragments/ArticlesContainer";
 import BannerImage from "@/components/Fragments/BannerImage";
 
+export const metadata = {
+  title: "About - Peacegraph",
+  description: "Solusi percetakan digital",
+};
+
 export default function Page() {
   return (
     <div className="mt-20">
