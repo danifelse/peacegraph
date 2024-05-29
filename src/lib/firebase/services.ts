0965e33cedecf8ac6e6542a79677cb0a2ccea1f6
danimisme <msubhanr53@gmail.com>
2024-05-29 13:37:41 +0700

    create updateuser function

--- src/lib/firebase/services.ts
+++ src/lib/firebase/services.ts
@@ -203,6 +203,17 @@ export async function getUserDetails(id: string) {
     }
 }
 
+export async function updateUser(id: string, userData: User): Promise<boolean> {
+    const q = query(collection(firestore, "users"), where("id", "==", id));
+    const snapshot = await getDocs(q);
+    if (!snapshot.empty) {
+        const docRef = doc(firestore, "users", snapshot.docs[0].id);
+        await updateDoc(docRef, userData);
+        return true;
+    } else {
+        return false;
+    }
+}
 
 export async function deleteUser(id: string): Promise<boolean> {
     const q = query(collection(firestore, "users"), where("id", "==", id));
