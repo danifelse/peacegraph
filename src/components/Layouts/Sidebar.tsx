54ccf259b7f08e8a83b04ec121ac8713f7dfa4df
danimisme <msubhanr53@gmail.com>
2024-05-29 10:33:38 +0700

    delete message button in side bar

--- src/components/Layouts/Sidebar.tsx
+++ src/components/Layouts/Sidebar.tsx
@@ -30,21 +30,7 @@ export default function Sidebar() {
               <span className="ms-3">Dashboard</span>
             </Link>
           </li>
-          <li>
-            <Link
-              href="/dashboard/message"
-              className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${
-                pathname.startsWith("/dashboard/message") &&
-                "bg-gray-200 dark:bg-gray-700"
-              }`}
-            >
-              <MdMessage className="w-6 h-6" />
-              <span className="flex-1 ms-3 whitespace-nowrap">Message</span>
-              <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
-                1
-              </span>
-            </Link>
-          </li>
+
           <li>
             <Link
               href="/dashboard/users"
