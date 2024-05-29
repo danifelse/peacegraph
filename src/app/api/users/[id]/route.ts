7197a6536c83e43cc86658128188fafd7a7ba157
danimisme <msubhanr53@gmail.com>
2024-05-29 12:08:33 +0700

    add api handler get user by id

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,4 +1,4 @@
-import { deleteUser } from "@/lib/firebase/services";
+import { deleteUser, getUserDetails, retreiveDataBySlug } from "@/lib/firebase/services";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE( req: NextRequest, { params }: { params: { id: string } } ) {
@@ -16,5 +16,22 @@ export async function DELETE( req: NextRequest, { params }: { params: { id: stri
         return NextResponse.json({ status: 200, message: 'User deleted successfully' });
     } else {
         return new Response(JSON.stringify({ error: 'Failed to delete User' }), { status: 404, headers: { 'Content-Type': 'application/json' }});
+    }
+}
+
+export async function GET( req: NextRequest, { params }: { params: { id: string } } ) {
+    const apiKey = req.headers.get('apiKey');
+    const validApiKey = process.env.API_KEY 
+
+    if (!apiKey || apiKey !== validApiKey) {
+        return NextResponse.json({ error: 'Unauthorized'  }, { status: 401 });
+    }
+
+    const id = params.id
+    const data = await getUserDetails(id);
+    if (data) {
+        return NextResponse.json({ status: 200, message:"Success", data: data });
+    } else {
+        return new Response(JSON.stringify({ error: 'User not found' }), { status: 404, headers: { 'Content-Type': 'application/json' }});
     }
 }
