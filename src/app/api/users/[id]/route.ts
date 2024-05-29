3c138ee295c9f8259b2f3d3d98a8199ef3910487
danimisme <msubhanr53@gmail.com>
2024-05-29 13:38:20 +0700

    create api route to PUT user

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,4 +1,4 @@
-import { deleteUser, getUserDetails, retreiveDataBySlug } from "@/lib/firebase/services";
+import { deleteUser, getUserDetails, retreiveDataBySlug, updateUser } from "@/lib/firebase/services";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE( req: NextRequest, { params }: { params: { id: string } } ) {
@@ -35,3 +35,19 @@ export async function GET( req: NextRequest, { params }: { params: { id: string 
         return new Response(JSON.stringify({ error: 'User not found' }), { status: 404, headers: { 'Content-Type': 'application/json' }});
     }
 }
+
+export async function PUT ( req: NextRequest, { params }: { params: { id: string } } ) {
+    const apiKey = req.headers.get('apiKey');
+    const validApiKey = process.env.API_KEY 
+    if (!apiKey || apiKey !== validApiKey) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+    const id = params.id
+    const newData = await req.json();
+    const status = await updateUser(id, newData);
+    if (status) {
+        return NextResponse.json({ status: 200, message: 'User updated successfully', data: newData });
+    } else {
+        return new Response(JSON.stringify({ error: 'Failed to update user' }), { status: 404, headers: { 'Content-Type': 'application/json' }});
+    }
+}   
