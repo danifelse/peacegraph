6b058add79e3ee20e25033a2c0acd65f01ab9d09
danimisme <msubhanr53@gmail.com>
2024-06-14 13:48:01 +0700

    hide category section if products length < 5

--- src/components/Fragments/CategorySection.tsx
+++ src/components/Fragments/CategorySection.tsx
@@ -15,6 +15,13 @@ export default function CategorySection({
 }) {
   AOSInit();
 
+  if (
+    products.filter((product) => product.category === category.slug).length < 5
+  ) {
+    console.log(products);
+    console.log(category);
+    return null;
+  }
   return (
     <div className="mx-auto max-w-7xl px-4 sm:px-6 my-10">
       <div className="flex ">
